c9436d048bc890bb730b97cb40390cb55d088cd5
Dennis Mwea <mweadennis2@gmail.com>
2021-04-03 01:03:18 +0300

    Fix css bug

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,10 +7,10 @@ window._ = require('lodash');
  */
 
 try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
+    // window.Popper = require('popper.js').default;
+    // window.$ = window.jQuery = require('jquery');
+    //
+    // require('bootstrap');
 } catch (e) {}
 
 /**
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import')
+        require('postcss-import'),
+        require('tailwindcss'),
     ])
     .sourceMaps()
-    .webpackConfig(require('./webpack.config.js'))
-    .version();
+    .webpackConfig(require('./webpack.config.js'));
