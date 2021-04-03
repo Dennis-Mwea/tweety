0fb69cc6c96059727b15d4275cbafd6abb9c70a3
Dennis Mwea <mweadennis2@gmail.com>
2021-04-03 19:02:42 +0300

    Group notifications by date

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -6,9 +6,16 @@ class NotificationsController extends Controller
 {
     public function index()
     {
+        current_user()->unreadNotifications->markAsRead();
         return view('notifications.index', [
-            'readNotifications' => current_user()->readNotifications,
-            'unreadNotifications' => current_user()->unReadNotifications,
+            'readNotifications' => current_user()->readNotifications
+                ->groupBy(function ($notification) {
+                    return $notification->created_at->format('d M y');
+                }),
+            'unreadNotifications' => current_user()->unReadNotifications
+                ->groupBy(function ($notification) {
+                    return $notification->created_at->format('d M y');
+                }),
         ]);
     }
 }
