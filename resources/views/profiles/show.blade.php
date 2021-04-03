3e62deae24f72abb6a3ff86e968523e38bb1a439
Dennis Mwea <mweadennis2@gmail.com>
2021-04-03 04:03:33 +0300

    Add missing images

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,7 +1,7 @@
 <x-app>
     <header class="mb-6 relative">
         <div class="relative">
-            <img src="/images/default-profile-banner.jpg" alt="profile banner" class="rounded-lg mb-2">
+            <img src="/images/default-profile-banner.jpeg" alt="profile banner" class="rounded-lg mb-2">
 
             <img src="{{ $user->avatar }}" alt=""
                  class="rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2" width="150"
