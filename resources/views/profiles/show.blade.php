f2b6c878cd48b4e9c30b1505e99c6fab58028334
Dennis Mwea <mweadennis2@gmail.com>
2021-04-03 04:16:32 +0300

    Fix autorization bug

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -15,7 +15,7 @@
             </div>
 
             <div>
-                @can('edit')
+                @can('edit', $user)
                     <a href="{{ $user->path('edit') }}"
                        class="bg-white rounded-full border border-gray-300 py-2 px-4 text-black text-xs mr-2">Edit
                         Profile</a>
