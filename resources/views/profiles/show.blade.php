74a91add25f2dfafa967301c5803ca89e1c79a76
Dennis Mwea <mweadennis2@gmail.com>
2021-04-03 04:18:26 +0300

    Code clean up

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -9,7 +9,7 @@
         </div>
 
         <div class="flex justify-between items-center mb-4">
-            <div>
+            <div style="max-width: 270px">
                 <h2 class="font-bold text-2xl mb-0">{{ $user->name }}</h2>
                 <p class="text-sm">Joined {{ $user->created_at->diffForHumans() }}</p>
             </div>
