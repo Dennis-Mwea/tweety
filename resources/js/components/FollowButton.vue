66c58ec43b074cac299a735e0667427d75aa649d
Dennis Mwea <mweadennis2@gmail.com>
2021-04-03 05:39:43 +0300

    Fix lose ends

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "FollowButton"
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -2,24 +2,22 @@
     <section class="px-8">
         <main class="container mx-auto">
             <div class="lg:flex lg:justify-between">
-
-                @if(auth()->check())
+                @auth
                     <div class="lg:w-32">
                         @include('tweets/_sidebar-links')
 
                     </div>
-                @endif
+                @endauth
 
                 <div class="lg:flex-1 lg:mx-10 lg:mb-10" style="max-width: 700px">
                     {{ $slot }}
                 </div>
 
-                @if(auth()->check())
+                @auth
                     <div class="lg:w-64">
                         @include('tweets/_friends-list')
                     </div>
-                @endif
-
+                @endauth
             </div>
         </main>
     </section>
