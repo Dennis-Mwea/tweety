35d34652cd55dfba5e0618dad67385adacd0d212
Dennis Mwea <mweadennis2@gmail.com>
2021-04-03 18:37:33 +0300

    Tweak notifications view

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -7,7 +7,8 @@ class NotificationsController extends Controller
     public function index()
     {
         return view('notifications.index', [
-            'notifications' => current_user()->notifications,
+            'readNotifications' => current_user()->readNotifications,
+            'unreadNotifications' => current_user()->unReadNotifications,
         ]);
     }
 }
--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -1,18 +1,38 @@
 <x-app>
-    @forelse($notifications as $notification)
-        <div class="flex bg-white shadow p-4 rounded-xl mb-4 border border-gray-300">
-            <div class="flex w-full">
-                @include("notifications._".get_notification_view($notification->type))
+    @if ($unreadNotifications->count())
+        <h1 class="text-gray-800 font-bold text-xl mb-4">New Notifications</h1>
 
-                <div class="flex-1 text-right tracking-tight">
-                    <h3 class="font-semibold broder border-r-4 {{ get_notification_color($notification->type) }}  p-2 rounded">
-                        {{ $notification->created_at->format('M d') }} </h3>
-                    <span
-                        class="text-gray-700 text-sm font-bold">{{ $notification->created_at->diffForHumans() }}</span>
+        @foreach ($unreadNotifications as $unreadNotification)
+            <div class="flex bg-white shadow p-4 rounded-xl mb-4 border border-gray-300">
+                <div class="flex w-full">
+                    @include("notifications._".get_notification_view($unreadNotification->type),['notification' => $unreadNotification])
+
+                    <div class="flex-1 text-right tracking-tight">
+                        <h3 class="font-semibold broder border-r-4 {{ get_notification_color($unreadNotification->type) }} p-2">{{ $unreadNotification->created_at->format('M d') }} </h3>
+                        <span
+                            class="text-gray-700 text-sm font-bold">{{ $unreadNotification->created_at->diffForHumans() }}</span>
+                    </div>
+
                 </div>
             </div>
-        </div>
-    @empty
-        <p>You have no notifications at the moment.</p>
-    @endforelse
+        @endforeach
+    @endif
+
+    @if ($readNotifications->count())
+        <h1 class="text-gray-800 font-bold text-xl mb-4">Older</h1>
+
+        @foreach ($readNotifications as $readNotification)
+            <div class="flex bg-white shadow p-4 rounded-xl mb-4 border border-gray-300">
+                <div class="flex w-full">
+                    @include("notifications._".get_notification_view($readNotification->type),['notification' => $readNotification])
+
+                    <div class="flex-1 text-right tracking-tight">
+                        <h3 class="font-semibold broder border-r-4 {{ get_notification_color($readNotification->type) }} p-2">{{ $readNotification->created_at->format('M d') }} </h3>
+                        <span
+                            class="text-gray-700 text-sm font-bold">{{ $readNotification->created_at->diffForHumans() }}</span>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    @endif
 </x-app>
