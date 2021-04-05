722077b5fb6d0417590e4660b6aae56ed744a81f
Dennis Mwea <mweadennis2@gmail.com>
2021-04-06 01:28:57 +0300

    Use tailwind pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -27,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
+
+        Paginator::useTailwind();
     }
 }
